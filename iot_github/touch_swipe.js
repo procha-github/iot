bb24bdb669dbf682adbf7da84a9c89db0b2d0bed
Pedro Rocha <63567695+procha-github@users.noreply.github.com>
2020-05-04 21:14:04 +0100

    Delete touch_swipe.js

--- iot_github/touch_swipe.js
+++
@@ -1,8 +0,0 @@
-var registerInteraction = function () {
-        'use strict';
-        window.sampleCompleted('index.html-SwipeFrontTouch');
-      };
-      var swipeFronts = document.querySelectorAll('.swipe-front');
-      for(var i = 0; i < swipeFronts.length; i++) {
-        swipeFronts[i].addEventListener('touchstart', registerInteraction);
-      }
